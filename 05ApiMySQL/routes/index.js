aa7e1688e45d3a7806167ed18cd6f378da780815
JoselynDRF <mariajoselyndosramos@yahoo.es>
2018-03-01 18:09:23 +0000

    UPDATE movie

--- 05ApiMySQL/routes/index.js
+++ 05ApiMySQL/routes/index.js
@@ -57,6 +57,47 @@ router
     })
   })
 
+  .get('/editar/:movie_id', (req, res, next) => {
+    let movie_id = req.params.movie_id;
+    console.log(movie_id);
+
+    req.getConnection((err, movies) => {
+      movies.query('SELECT * FROM movie WHERE movie_id = ?', movie_id, (err, rows) => {
+        console.log(err, '---', rows);
+
+        if(err) {
+          throw(err);
+        } else {
+          let locals = {
+            title: 'Editar Película',
+            data: rows
+          }
+
+          res.render('edit-movie', locals);
+        }
+      })
+    })
+  })
+
+  .post('/actualizar/:movie_id', (req, res, next) => {
+    req.getConnection((err, movies) => {
+      let movie = {
+        movie_id: req.body.movie_id,
+        title: req.body.title,
+        release_year: req.body.release_year,
+        rating: req.body.rating,
+        image: ''
+      }
+
+      console.log(movie);
+
+      movies.query('UPDATE movie SET ? WHERE movie_id = ?', [movie, movie.movie_id], (err, rows) => {
+        return (err) ? res.redirect('/editar/:movie_id') : res.redirect('/');
+      })
+    })
+  })
+  
+
 router.use(error404);
 
 module.exports = router;
